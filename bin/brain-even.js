10e31c0dffd1e3c054db7567ed33651636f85043
Leonid <leonid.mezhonov@gmail.com>
2022-04-25 12:29:03 +0200

    update code brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const game = () => {
-  console.log("Welcome to the Brain Games!");
-  const name = readlineSync.question("May I have your name? ");
-  console.log(` Hello, ${name}! `);
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   function getRandomInt() {
@@ -13,12 +13,12 @@ const game = () => {
   for (let i = 0; i < 3; i += 1) {
     const number = getRandomInt();
     console.log(`Question: ${number}`);
-    const yourAnswer = readlineSync.question("Your answer: ");
+    const yourAnswer = readlineSync.question('Your answer: ');
 
     // if the number is even
     if (number % 2 === 0) {
       if (yourAnswer === 'yes') {
-        console.log("Correct!");
+        console.log('Correct!');
       } else {
         console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was 'yes'.`);
         console.log(`Let's try again, ${name}`);
@@ -26,7 +26,7 @@ const game = () => {
       }
     } else {
       if (yourAnswer === 'no') {
-        console.log("Correct!");
+        console.log('Correct!');
       } else {
         console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was 'no'.`);
         console.log(`Let's try again, ${name}`);
