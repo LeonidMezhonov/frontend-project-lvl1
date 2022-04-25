a4144208bef99a8e8d77cc80a13fd6f1272160f8
Leonid <leonid.mezhonov@gmail.com>
2022-04-25 10:39:36 +0200

    correct the code in brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,59 +1,57 @@
 import readlineSync from 'readline-sync';
-const game = () => {
-    console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-const getRandomInt = () => Math.floor(Math.random() * 100);
-for (let i = 0; i < 3; i++) {
+const game = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const getRandomInt = () => Math.floor(Math.random() * 100);
+  for (let i = 0; i < 3; i += 1) {
     const number = getRandomInt();
     console.log(`Question: ${number}`);
     const yourAnswer = readlineSync.question('Your answer: ');
 
     // if the number is even
-    if (number % 2 == 0) {
-        if (yourAnswer === 'yes') {
-        console.log("Correct!");
-        }
+    if (number % 2 === 0) {
+      if (yourAnswer === 'yes') {
+        console.log('Correct!');
+      }
 
-        else if (yourAnswer === 'no') {
+      else if (yourAnswer === 'no') {
         console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
         console.log(`Let's try again, ${name}`);
         return;
-        }  
+      }
 
-        else {
+      else {
         console.log('Incorrect data entry!');
         console.log(`Let's try again, ${name}`);
         return;
-        }  
+      }
     }
 
     // if the number is odd
     else {
-        if (yourAnswer === 'no') {
+      if (yourAnswer === 'no') {
         console.log("Correct!");
-        }
+      }
 
-        else if (yourAnswer === 'yes') {
+      else if (yourAnswer === 'yes') {
         console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
         console.log(`Let's try again, ${name}`);
         return;
-        }
+      }
         
-        else {
+      else {
         console.log('Incorrect data entry!');
         console.log(`Let's try again, ${name}`);
         return;
-        }   
+      }
     }
-}
+  }
   
-console.log(`Congratulations, ${name}!`);
-}; 
+  console.log(`Congratulations, ${name}!`);
+};
 
 game();
-
-  
-  
